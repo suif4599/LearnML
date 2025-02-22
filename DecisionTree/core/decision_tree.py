35a04a778642999a5439ee9015d5de5cdb7d5050
suif4599 <2673643436@qq.com>
2025-02-22 14:53:27 +0800

    add more info on decision tree

--- DecisionTree/core/decision_tree.py
+++ DecisionTree/core/decision_tree.py
@@ -3,6 +3,7 @@ from numpy.typing import NDArray
 from sklearn.utils import Bunch
 from collections.abc import Callable, Sequence
 from typing import TypeAlias
+from itertools import combinations
 
 Attribute: TypeAlias = Callable[[NDArray], NDArray] # in_shape: (n_samples, n_features) -> out_shape: (n_samples,) int
 
@@ -34,11 +35,23 @@ class Node:
 class DecisionTree:
     data: NDArray # shape: (n_samples, n_features)
     target: NDArray # shape: (n_samples,), non-negative integers
+    gain: Callable[[NDArray, NDArray, Attribute], float]
+    split: Callable[[NDArray, NDArray], set[Attribute]]
 
-    def __init__(self, databaset: Bunch):
+    def __init__(self, databaset: Bunch, 
+                 gain: Callable[[NDArray, NDArray, Attribute], float] = None,
+                 split: Callable[[NDArray, NDArray], set[Attribute]] = None):
+        if gain is not None:
+            self.gain = gain
+        else:
+            self.gain = DecisionTree.info_gain
+        if split is not None:
+            self.split = split
+        else:
+            self.split = DecisionTree.mean_split(4)
         self.data = databaset.data
         self.target = databaset.target
-        self.attrs = DecisionTree.gen_attr(self.data, 4)
+        self.attrs = self.split(self.data, self.target)
         self.tree = self.generate(self.data, self.target, self.attrs)
 
     def generate(self, data: NDArray, target: NDArray, attributes: set[Attribute]) -> Node:
@@ -47,7 +60,7 @@ class DecisionTree:
             return Node(lambda x: np.full(x.shape[0], target[0]))
         if not attributes:
             return Node(lambda x: np.full(x.shape[0], np.argmax(np.bincount(target))))
-        best_attr = min(attributes, key=lambda attr: DecisionTree.gain(data, target, attr))
+        best_attr = min(attributes, key=lambda attr: self.gain(data, target, attr))
         attributes.remove(best_attr)
         characteristic = best_attr(data)
         return Node(best_attr,
@@ -62,10 +75,20 @@ class DecisionTree:
     
     def predict(self, data: NDArray) -> int:
         return self.tree.pridict(data)
+    
+    def score(self, data: NDArray, target: NDArray) -> float:
+        cnt = 0
+        for d, tar in zip(data, target):
+            p = self.predict(d)
+            if p == tar:
+                cnt += 1
+        return cnt / data.shape[0]
 
     @staticmethod
-    def gain(data: NDArray, target: NDArray, attr: Attribute) -> float:
+    def info_gain(data: NDArray, target: NDArray, attr: Attribute) -> float:
         characteristic = attr(data)
+        if np.all(characteristic == characteristic[0]):
+            return 0.
         return DecisionTree.entropy(target) - \
             np.sum([DecisionTree.entropy(target[characteristic == i]) * np.sum(characteristic == i) \
                     for i in np.unique(characteristic)]) / data.shape[0]
@@ -77,11 +100,71 @@ class DecisionTree:
         return -np.sum(p * np.log2(p))
     
     @staticmethod
-    def gen_attr(data: NDArray, splits: int) -> set[Attribute]:
-        ret = set()
-        for i in range(data.shape[1]):
-            attr = NamedAttribute(f"Attribute {i}, {np.linspace(data[:, i].min(), data[:, i].max(), splits)}", 
-                                  lambda x: np.digitize(x[:, i], np.linspace(data[:, i].min(), data[:, i].max(), splits)) - 1)
-            ret.add(attr)
-        return ret
+    def IV(data: NDArray, target: NDArray, attr: Attribute) -> float:
+        characteristic = attr(data)
+        p = np.bincount(characteristic) / data.shape[0]
+        p = p[p > 0]
+        return -np.sum(p * np.log2(p))
 
+    @staticmethod
+    def gain_rate(data: NDArray, target: NDArray, attr: Attribute) -> float:
+        characteristic = attr(data)
+        if np.all(characteristic == characteristic[0]):
+            return 0.
+        return DecisionTree.info_gain(data, target, attr) / DecisionTree.IV(data, target, attr)
+    
+    @staticmethod
+    def neg_gini_index(data: NDArray, target: NDArray, attr: Attribute) -> float:
+        characteristic = attr(data)
+        if np.all(characteristic == characteristic[0]):
+            return 0.
+        def p(target: NDArray) -> float:
+            return np.bincount(target) / target.shape[0]
+        return np.sum(np.sum(characteristic == i) * np.sum(p(target[characteristic == i]) ** 2) \
+                      for i in np.unique(characteristic)) / data.shape[0]
+    
+    @staticmethod
+    def mean_split(splits: int) -> Callable[[NDArray, NDArray], set[Attribute]]:
+        def mean_split_inner(data: NDArray, target: NDArray) -> set[Attribute]:
+            ret = set()
+            for i in range(data.shape[1]):
+                attr = NamedAttribute(f"Mean split on Attribute {i}, {np.linspace(data[:, i].min(), data[:, i].max(), splits)}", 
+                                    lambda x: np.digitize(x[:, i], np.linspace(data[:, i].min(), data[:, i].max(), splits)) - 1)
+                ret.add(attr)
+            return ret
+        return mean_split_inner
+    
+    @staticmethod
+    def percentile_split(splits: int) -> Callable[[NDArray, NDArray], set[Attribute]]:
+        def percentile_split_inner(data: NDArray, target: NDArray) -> set[Attribute]:
+            ret = set()
+            for i in range(data.shape[1]):
+                attr = NamedAttribute(f"Cluster split on Attribute {i}", 
+                                    lambda x: np.digitize(x[:, i], np.percentile(data[:, i], range(0, 101, 100 // splits))) - 1)
+                ret.add(attr)
+            return ret
+        return percentile_split_inner
+    
+    @staticmethod
+    def split_based_on_gain(splits: int, gain: Callable[[NDArray, NDArray, Attribute], float]) -> Callable[[NDArray, NDArray], set[Attribute]]:
+        k = 20
+        def split_based_on_gain_inner(data: NDArray, target: NDArray) -> set[Attribute]:
+            ret = set()
+            for i in range(data.shape[1]):
+                midpts = (data[:, i][1:] + data[:, i][:-1]) / 2
+                n = midpts.shape[0]
+                mask = np.bincount([n * i // k for i in range (1, k)])
+                mask = np.pad(mask, (0, n - mask.shape[0]), 'constant', constant_values=(0, 0)) == 1
+                midpts = midpts[mask]
+                midpts.sort()
+                cms = tuple(combinations(midpts, splits))
+                mapping = []
+                for midpt in cms:
+                    mapping.append(gain(data, target, 
+                                        lambda x: np.digitize(x[:, i], midpt)))
+                midpt = cms[np.argmax(mapping)]
+                attr = NamedAttribute(f"Split on Attribute {i} > {midpt}", 
+                                    lambda x: np.digitize(x[:, i], midpt))
+                ret.add(attr)
+            return ret
+        return split_based_on_gain_inner
--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -12,16 +12,28 @@ train_data = data[mask]
 train_target = target[mask]
 test_data = data[~mask]
 test_target = target[~mask]
+splits = 3
 tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target))
-cnt = 0
-for data, target in zip(test_data, test_target):
-    p = tree.predict(data)
-    if p == target:
-        cnt += 1
-print(f"My acc: {cnt / test_data.shape[0]}")
-
+print(f"info_gain mean_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.gain_rate)
+print(f"gain_rate mean_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.neg_gini_index)
+print(f"gini_index mean_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.info_gain, DecisionTree.percentile_split(splits))
+print(f"info_gain percentile_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.gain_rate, DecisionTree.percentile_split(splits))
+print(f"gain_rate percentile_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.neg_gini_index, DecisionTree.percentile_split(splits))
+print(f"gini_index percentile_split acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.info_gain, DecisionTree.split_based_on_gain(splits, DecisionTree.neg_gini_index))
+print(f"info_gain split_based_on_gain acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.gain_rate, DecisionTree.split_based_on_gain(splits, DecisionTree.neg_gini_index))
+print(f"gain_rate split_based_on_gain acc: {tree.score(test_data, test_target):.3f}")
+tree = DecisionTree(namedtuple('Bunch', ['data', 'target'])(train_data, train_target), DecisionTree.neg_gini_index, DecisionTree.split_based_on_gain(splits, DecisionTree.neg_gini_index))
+print(f"gini_index split_based_on_gain acc: {tree.score(test_data, test_target):.3f}")
+# print(tree.score(train_data, train_target))
 
 from sklearn.tree import DecisionTreeClassifier
 clf = DecisionTreeClassifier()
 clf.fit(train_data, train_target)
-print(f"Sklearn acc: {clf.score(test_data, test_target)}")
+print(f"Sklearn acc: {clf.score(test_data, test_target):.3f}")
